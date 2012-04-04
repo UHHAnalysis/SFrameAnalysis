3f165971741fc07100e66576730385a236771ee6
peiffer <>
2012-04-04 14:45:09 +0000

    lib name

--- proof/SETUP.C
+++ proof/SETUP.C
@@ -1,10 +1,12 @@
-// $Id$
+// $Id: SETUP.C,v 1.1 2012/04/02 15:28:00 peiffer Exp $
 
 int SETUP() {
 
    /// Add all the additional libraries here that this package
    /// depends on. (With the same command that loads this package's library...)
-   if( gSystem->Load( "libMyAnalysis" ) == -1 ) return -1;
-
+   if( gSystem->Load( "libTree" ) == -1 ) return -1;
+   if( gSystem->Load( "libHist" ) == -1 ) return -1;
+   if( gSystem->Load( "libGraf" ) == -1 ) return -1;
+   if( gSystem->Load( "libAnalysis" ) == -1 ) return -1;
    return 0;
 }

427793ede21c0bdff683e0d4c460a0aa2395aeed
peiffer <>
2012-04-24 08:27:45 +0000

    check timelike

--- src/Selection.cxx
+++ src/Selection.cxx
@@ -61,22 +61,34 @@ bool Selection::NTopTagSelection(int min_ntoptag){
 }
 
 bool Selection::TopTag(TopJet topjet,  double &mjet, int &nsubjets, double &mmin){
-
  
   nsubjets=topjet.numberOfDaughters;
 
   LorentzVector allsubjets(0,0,0,0);
+  
   for(int j=0; j<topjet.numberOfDaughters; ++j){
     allsubjets += topjet.subjets[j].v4();
   }
+  if(!allsubjets.isTimelike()){
+    mjet=0;
+    mmin=0;
+    return false;
+  }
+
   mjet = allsubjets.M();
   
   if(nsubjets>=3) {
     sort(topjet.subjets.begin(), topjet.subjets.end(), HigherPt());
     
-    double m01 = (topjet.subjets[0].v4()+topjet.subjets[1].v4()).M();
-    double m02 = (topjet.subjets[0].v4()+topjet.subjets[2].v4()).M();
-    double m12 = (topjet.subjets[1].v4()+topjet.subjets[2].v4()).M();
+    double m01 = 0;
+    if( (topjet.subjets[0].v4()+topjet.subjets[1].v4()).isTimelike()) 
+      m01=(topjet.subjets[0].v4()+topjet.subjets[1].v4()).M();
+    double m02 = 0;
+    if( (topjet.subjets[0].v4()+topjet.subjets[2].v4()).isTimelike() ) 
+      m02=(topjet.subjets[0].v4()+topjet.subjets[2].v4()).M();
+    double m12 = 0;
+    if( (topjet.subjets[1].v4()+topjet.subjets[2].v4()).isTimelike()  )
+      m12 = (topjet.subjets[1].v4()+topjet.subjets[2].v4()).M();
     
     //minimum pairwise mass > 50 GeV/c^2
     mmin = std::min(m01,std::min(m02,m12));

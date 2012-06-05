187f65cb41f0913a7ce3582689d5530bb5146b2e
rkogler <>
2012-06-05 14:46:00 +0000

    An example for a histogram collection

---
+++ include/ExampleHists.h
@@ -0,0 +1,46 @@
+#ifndef ExampleHists_H
+#define ExampleHists_H
+
+// ROOT include(s):
+#include <TObject.h>
+#include <TString.h>
+
+// Local include(s):
+#include "include/BaseHists.h"
+#include "include/BaseCycleContainer.h"
+
+/**
+ *   Example class for booking and filling histograms
+ *
+ *   This class books and fills a collection of histograms.
+ *   It should have a unique name, such that the histograms
+ *   of multiple instances of this class are ordered in the
+ *   output file. 
+ *   Always sort your histograms and used methods topically.
+ *   This example collection can be used for data and reconstructed
+ *   MC events.
+ *   
+ *   @version $Revision: 0 $
+ */
+
+class ExampleHists : public BaseHists {
+
+public:
+   /// Named constructor
+   ExampleHists(const char* name);
+
+   /// Default destructor
+   ~ExampleHists();
+
+   void Init();
+
+   void Fill();
+
+   void Finish();
+
+private:
+
+}; // class ExampleHists
+
+
+#endif // ExampleHists_H

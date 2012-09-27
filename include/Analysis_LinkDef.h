b3aee3fa0f6ada9adf681c6feb7df7d44aefea8e
mmeyer <>
2012-09-27 15:59:24 +0000

    new histogram classes

--- include/Analysis_LinkDef.h
+++ include/Analysis_LinkDef.h
@@ -1,5 +1,5 @@
 // Dear emacs, this is -*- c++ -*-
-// $Id: Analysis_LinkDef.h,v 1.14 2012/06/22 13:14:11 peiffer Exp $
+// $Id: Analysis_LinkDef.h,v 1.15 2012/08/02 08:14:37 peiffer Exp $
 #ifdef __CINT__
 
 #pragma link off all globals;
@@ -15,12 +15,24 @@
 
 #pragma link C++ class AnalysisCycle+;
 #pragma link C++ class PileUpHistoCycle+;
-//#pragma link C++ class LeptoquarkCycle+;
+#pragma link C++ class LeptoquarkCycle+;
+#pragma link C++ class LeptoquarkPreSelectionCycle+;
 #pragma link C++ class TauEffiCycle+;
 #pragma link C++ class ExampleCycle+;
+#pragma link C++ class EfficienciesHists;
+#pragma link C++ class MistagRateHists;
+#pragma link C++ class GenparticleHists;
+#pragma link C++ class BoostedTopHists;
+#pragma link C++ class LQInvMassHists;
+#pragma link C++ class TopJetHists;
+#pragma link C++ class JetHists;
+#pragma link C++ class EventHists;
+#pragma link C++ class ElectronHists;
+#pragma link C++ class MuonHists;
 #pragma link C++ class JetLeptonCleanerCycle+;
 #pragma link C++ class ZprimePreSelectionCycle+;
 #pragma link C++ class ZprimeSelectionCycle+;
 #pragma link C++ class TriggerEffiCycle+;
+
 
 #endif // __CINT__

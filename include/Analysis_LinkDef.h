ab8a3be54814adc42bba91508cc482996c53d296
peiffer <>
2012-09-28 09:45:53 +0000

    removed non-existing files from LinkDef

--- include/Analysis_LinkDef.h
+++ include/Analysis_LinkDef.h
@@ -1,5 +1,5 @@
 // Dear emacs, this is -*- c++ -*-
-// $Id: Analysis_LinkDef.h,v 1.15 2012/08/02 08:14:37 peiffer Exp $
+// $Id: Analysis_LinkDef.h,v 1.16 2012/09/27 15:59:24 mmeyer Exp $
 #ifdef __CINT__
 
 #pragma link off all globals;
@@ -15,20 +15,10 @@
 
 #pragma link C++ class AnalysisCycle+;
 #pragma link C++ class PileUpHistoCycle+;
-#pragma link C++ class LeptoquarkCycle+;
-#pragma link C++ class LeptoquarkPreSelectionCycle+;
+//#pragma link C++ class LeptoquarkCycle+;
+//#pragma link C++ class LeptoquarkPreSelectionCycle+;
 #pragma link C++ class TauEffiCycle+;
 #pragma link C++ class ExampleCycle+;
-#pragma link C++ class EfficienciesHists;
-#pragma link C++ class MistagRateHists;
-#pragma link C++ class GenparticleHists;
-#pragma link C++ class BoostedTopHists;
-#pragma link C++ class LQInvMassHists;
-#pragma link C++ class TopJetHists;
-#pragma link C++ class JetHists;
-#pragma link C++ class EventHists;
-#pragma link C++ class ElectronHists;
-#pragma link C++ class MuonHists;
 #pragma link C++ class JetLeptonCleanerCycle+;
 #pragma link C++ class ZprimePreSelectionCycle+;
 #pragma link C++ class ZprimeSelectionCycle+;

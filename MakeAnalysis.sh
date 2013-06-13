ee45f2bfb425b5767e3ef0660cca11416fce1c9a
peiffer <>
2013-06-13 15:40:58 +0000

    automatic upload to github enabled

--- MakeAnalysis.sh
+++ MakeAnalysis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#usage: source MakeAnalysis.sh <AnalysisName>
+#usage: source MakeAnalysis.sh <AnalysisName> <Github username> 
 
 export ANALYSISNAME=$1
 
@@ -20,4 +20,17 @@ mv src/ExampleHists.cxx src/${ANALYSISNAME}Hists.cxx
 mv src/ExampleCycle.cxx src/${ANALYSISNAME}Cycle.cxx
 mv config/ExampleCycle_config.xml config/${ANALYSISNAME}_config.xml
 mv include/ExampleAnalysis_LinkDef.h include/${ANALYSISNAME}Analysis_LinkDef.h
-#make -j 2
+
+if [ "$#" -eq 2 ] ; then
+    
+    git init
+    #curl -F 'login=$2' -F 'token=$3' https://github.com/api/v3/json/repos/create -F 'name=${ANALYSISNAME}Analysis'
+    curl -u $2 https://api.github.com/user/repos -d '{"name":"'${ANALYSISNAME}Analysis'"}'
+    git remote add origin https://github.com/$2/${ANALYSISNAME}Analysis.git
+    #git update-server-info
+    git add *
+    git commit -m "first commit"
+    git push origin master 
+fi
+
+make -j 2

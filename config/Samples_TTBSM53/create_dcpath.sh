d3bf4e1a3c306fec27b262a9911d14bb6c489842
Daniel Gonzalez <daniel.gonzalez@desy.de>
2013-11-04 15:59:48 +0100

    script for xml files

---
+++ config/Samples_TTBSM53/create_dcpath.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export dir=$1
+export name=$2
+
+echo ' hi starting to fill xml'
+
+for i in $( dcls $1 | grep $2 );
+do
+  echo '<In FileName="dcap://dcache-cms-dcap.desy.de/'$1'/'$i'" Lumi="0.0"/>' >> $2.xml
+done
+
+echo 'xml should be ready'

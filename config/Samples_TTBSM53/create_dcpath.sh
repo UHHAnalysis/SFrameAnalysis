2be37e546e5b97296e6d8426669f4f944ff40280
Daniel Gonzalez <gonvaq@nafhh-cms01.desy.de>
2014-04-14 14:41:32 +0200

    dcls did not work

--- config/Samples_TTBSM53/create_dcpath.sh
+++ config/Samples_TTBSM53/create_dcpath.sh
@@ -5,7 +5,7 @@ export name=$2
 
 echo ' hi starting to fill xml'
 
-for i in $( dcls $1 | grep $2 );
+for i in $( lcg-ls $1 | grep $2 );
 do
   echo '<In FileName="dcap://dcache-cms-dcap.desy.de/'$1'/'$i'" Lumi="0.0"/>' >> $2.xml
 done

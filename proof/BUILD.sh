7752f17c3757d18ef02e81238fa8996b267bf35c
Thomas Peiffer <thomas.peiffer@desy.de>
2014-05-20 15:31:44 +0200

    PoD compatible scripts

--- proof/BUILD.sh
+++ proof/BUILD.sh
@@ -1,13 +1,4 @@
 # $Id: BUILD.sh,v 1.1 2012/04/02 15:28:00 peiffer Exp $
 
-if [ "$1" = "clean" ]; then
-    make distclean
-    exit 0
-fi
+exit 0
 
-if [ "x$ROOTPROOFLITE" != "x" ]; then
-    echo "Running on PROOF-Lite, skipping build"
-    exit 0
-fi
-
-make default
--- proof/SETUP.C
+++ proof/SETUP.C
@@ -2,17 +2,21 @@
 
 int SETUP() {
 
-   /// Add all the additional libraries here that this package
-   /// depends on. (With the same command that loads this package's library...)   
-   if( gSystem->Load( "libfastjet" ) == -1 ) return -1;
-   if( gSystem->Load( "libfastjettools" ) == -1 ) return -1;
-   if( gSystem->Load( "libJetMETObjects" ) == -1 ) return -1;
-   if( gSystem->Load( "libTree" ) == -1 ) return -1;
-   if( gSystem->Load( "libHist" ) == -1 ) return -1;
-   if( gSystem->Load( "libGraf" ) == -1 ) return -1;
-   if( gSystem->Load( "libNtuple" ) == -1 ) return -1;
-   if( gSystem->Load( "libSFrameTools" ) == -1 ) return -1;
-   if( gSystem->Load( "libAnalysis" ) == -1 ) return -1;
+  /// Add all the additional libraries here that this package
+  /// depends on. (With the same command that loads this package's library...) 
+  if( gSystem->Load( "libTree" ) == -1 ) return -1; 
+  if( gSystem->Load( "libHist" ) == -1 ) return -1; 
+  if( gSystem->Load( "libXMLParser" ) == -1 ) return -1;  
+  if( gSystem->Load( "libProof" ) == -1 ) return -1;   
+  if( gSystem->Load( "libProofPlayer" ) == -1 ) return -1; 
+  if( gSystem->Load( "libSFrameCore" ) == -1 ) return -1; 
+  if( gSystem->Load( "libfastjet" ) == -1 ) return -1;
+  if( gSystem->Load( "libfastjettools" ) == -1 ) return -1;
+  if( gSystem->Load( "libJetMETObjects" ) == -1 ) return -1;
+  if( gSystem->Load( "libGraf" ) == -1 ) return -1;
+  if( gSystem->Load( "libSFrameTools" ) == -1 ) return -1;
+  if( gSystem->Load( "libNtuple" ) == -1 ) return -1;
+  if( gSystem->Load( "libAnalysis" ) == -1 ) return -1;
 
    return 0;
 }
